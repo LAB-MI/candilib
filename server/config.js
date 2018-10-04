a09e325a65df5ac4300cf9ce788ad1b6a1f90a1a
Philippe Bron <philippe@bron.ovh>
2018-10-04 18:36:20 +0200

    smtp conf

--- server/config.js
+++ server/config.js
@@ -3,6 +3,12 @@ const config = {
   port: process.env.PORT || 8000,
   host: process.env.HOST || 'http://localhost:8000',
   secret: process.env.SECRET || 'secret',
+  smtp_server: process.env.SMPT_SERVER || 'localhost',
+  smtpServer: '<smtp_server>',
+  smtpPort: 25,
+  mailUser: '<user>',
+  mailPassword: '<passwd>',
+  mailFrom: '<from>',
 };
 
 export default config;
