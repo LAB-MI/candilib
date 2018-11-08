a4ee801bb4e24f9bef614c1540310dcc71e1c5aa
baudoin.tran-econocom <baudoin.tran-econocom@interieur.gouv.fr>
2018-11-08 10:44:13 +0100

    Token au login - route api candidat

--- server/server.js
+++ server/server.js
@@ -195,6 +195,7 @@ app.use((req, res, next) => {
         );
       });
       if (childrenPrivateRoute !== undefined) {
+        console.log(childrenPrivateRoute);
         return res.redirect(
           302,
           '/auth?redirect=' + renderProps.location.pathname,
