46f3aa303a6f9c887067c3b7b4f57ffd8c28e195
christophe.lazantsy <=>
2018-11-09 18:47:53 +0100

    fix error

--- server/util/verifyToken.js
+++ server/util/verifyToken.js
@@ -17,6 +17,7 @@ export default function verifyToken(req, res, next) {
   }
 
   const token = req.headers['x-access-token'] || req.query.token;
+  if (!token) {
     if (res !== undefined) {
       return res.status(403).send({ auth: false, message: 'Pas de Token ' });
     }
