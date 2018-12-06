141aea8da4765face9ce4950d27d3e5f53bf294f
Philippe Bron <philippe@bron.ovh>
2018-12-06 21:38:01 +0100

    update calendar page
    wording places vs. événements
    mise en avant du lieu vs. date
    nom candidat dans entête carte candidat
    rectangles arrondis pour affichages places 
    vue par défaut mois pour un affichage d'ensemble en arrivant

--- client/components/calendar/messages.js
+++ client/components/calendar/messages.js
@@ -5,13 +5,13 @@ const messages = {
   today: 'aujourd\'hui',
   month: 'mois',
   week: 'semaine',
-  work_week: 'semaine de travail',
+  work_week: 'semaine',
   day: 'jour',
   agenda: 'Agenda',
   date: 'date',
   time: 'heure',
-  event: 'événement', // Or anything you want
-  showMore: total => `+ ${total} événement(s) supplémentaire(s)`,
+  event: 'place d\'examen', // Or anything you want
+  showMore: total => `+ ${total} place(s) supplémentaire(s)`,
 };
 
 export default messages;
