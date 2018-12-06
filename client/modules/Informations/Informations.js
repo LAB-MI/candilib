d1097029338a94b222dbb5987b562c722716c19b
baudoin.tran-econocom <baudoin.tran@econocom.com>
2018-12-06 13:51:55 +0100

    Mise à jours de FAQ

--- client/modules/Informations/Informations.js
+++ client/modules/Informations/Informations.js
@@ -22,73 +22,89 @@ const Informations = () => (
       <CardContent>
         <div className={styles.informations}>
           <h1>Foire Aux Questions</h1>
-          <p className={styles.informationsDate}>2018-06-20</p>
-          <h2>Qu'est-ce que CandiLib 93</h2>
+          <p className={styles.informationsDate}>modifié le 06/12/2018</p>
+          <h2>Qu'est-ce que CandiLib</h2>
+
           <p>
-            CandilLib 93 est le nouveau service de réservation en ligne des
-            places d'examen pratique du permis de conduire pour les candidats
-            libres domiciliés en Seine-Saint-Denis.
+            CandilLib est une expérimentation d'un nouveau service de
+            réservation en ligne des places d'examen pratique du permis de
+            conduire pour des candidats libres domiciliés en Seine-Saint-Denis.
           </p>
+
           <h2>Qu'est-ce qu'un candidat libre ?</h2>
+
           <p>
             Est défini réglementairement comme "candidat libre" tout candidat
             qui n'est pas présenté sur les droits à places d'une école de
             conduite et qui fait l'objet d'une convocation nominative de la part
             de l'administration.
           </p>
+
           <p>
-            Source réglementaire
-            {' '}
-            <a href="https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000029701643&dateTexte=20180625">
-              ici
+            Source réglementaire :{' '}
+            <a href="https://www.legifrance.gouv.fr/affichTexteArticle.do?idArticle=LEGIARTI000032966489&cidTexte=LEGITEXT000029705598&dateTexte=20181205">
+              https://www.legifrance.gouv.fr/affichTexteArticle.do?idArticle=LEGIARTI000032966489&cidTexte=LEGITEXT000029705598&dateTexte=20181205
             </a>
           </p>
+
           <h2>Qui peut utiliser le service ?</h2>
-          <p>Tous les "candidats libres" résidant dans le 93.</p>
+
+          <p>
+            Les "candidats libres" résidant dans le 93 qui ont reçu un mail
+            d'invitation pour participer à l'expérimentation.
+          </p>
+
           <h2>Comment ça marche ?</h2>
+
           <p>
             Vous avez reçu une invitation nominative par mail par le service des
             examens de la Seine-Saint-Denis à vous inscrire sur Candilib.
           </p>
-          <p>
-            <ol type="1">
-              <li>Cliquez sur le bouton réserver en page d'accueil.</li>
 
-              <li>Choisissez le centre d'examen.</li>
+          <h3>Pour s'inscrire :</h3>
+          <ul>
+            <li>Rendez vous sur la page d'accueil du service</li>
+            <li>
+              Déposez une demande d'inscription à Candilib en saisissant vos n°
+              NEPH (le numéro que vous avez reçu pour l'inscription au permis),
+              Nom, prénom, mail, téléphone et adresse postale.
+            </li>
+          </ul>
 
-              <li>
-                Sur le planning, sélectionnez une date, puis le créneau
-                disponible qui vous convient. (Si besoin déplacez-vous sur
-                d'autres mois).
-              </li>
-
-              <li>
-                Sur la page d'inscription, créez votre compte Candilib en
-                saisissant votre Nom, mail, téléphone, prénom, date de naissance
-                et n° NEPH (le numéro que vous avez reçu pour l'inscription au
-                permis).
-              </li>
-
-              <li>Confirmez la réservation.</li>
-            </ol>
-          </p>
-          <h2>Est-ce que je peux modifier ou annuler mon rendez-vous ?</h2>
-          <p>Vous pouvez annuler librement 7 jours avant la date prévue.</p>
-          <p>
-            Pour modifier un rendez-vous avant 7 jours, annulez le rendez-vous,
-            vous pourrez choisir un autre créneau disponible immédiatement.
-          </p>
-          <p>
-            Si vous devez annuler moins de 7 jours avant la date, vous serez placé
-            en liste d'attente non prioritaire.
-          </p>
+          <h3>Pour réserver :</h3>
+          <ul>
+            <li>
+              Connectez vous à Candilib après réception du mail "Validation de
+              votre inscription à Candilib" (lien de connexion dans le mail de
+              validation).
+            </li>
+            <li>
+              {' '}
+              Sur le planning, sélectionnez le créneau disponible à la date et
+              au centre d'examen qui vous conviennent (si besoin, déplacez-vous
+              sur les vues "mois", "semaine" et " jour").
+            </li>
+            <li> Confirmez votre réservation.</li>
+          </ul>
+          <h2>Est-ce que je peux modifier ou annuler mon rendez-vous ?</h2>
+          <ul>
+            <li>
+              Vous pouvez modifier votre rendez-vous en sélectionnant un nouveau
+              créneau 7 jours avant la date prévue.
+            </li>
+            <li>Vous pouvez annuler librement 7 jours avant la date prévue.</li>
+            <li>
+              Si vous devez annuler moins de 7 jours avant la date, vous serez
+              placé en liste d'attente non prioritaire.
+            </li>
+          </ul>
           <p>
             Merci de nous prévenir en cas d'empêchement afin de libérer le
             créneau pour un autre candidat.
           </p>
-          <h2>Quels sont les pré-requis le jour de l'examen ?</h2>
+          <h2>Quels sont les pré-requis le jour de l'examen ?</h2>
           <p>
-            Nous vous rappelons les éléments à vérifier le jour de l'examen :
+            Nous vous rappelons les éléments à vérifier le jour de l'examen :
           </p>
           <ul>
             <li>
@@ -97,28 +113,27 @@ const Informations = () => (
               de 2 rétroviseurs latéraux.
             </li>
             <li>
-                Votre accompagnateur sera :
-                <ul>
-
-                  <li>
-                    soit un enseignant de la conduite en possession de son
-                    autorisation d'enseigner pour la présenter à l'inspecteur,
-                  </li>
-                  <li>
-                    soit une personne dont le permis B est en cours de validité. Cette
-                    dernière devra remplir et signer la « charte de l’accompagnateur »
-                    (
-                    <a href="https://www.legifrance.gouv.fr/jo_pdf.do?id=JORFTEXT000036251681">
-                      https://www.legifrance.gouv.fr/jo_pdf.do?id=JORFTEXT000036251681
-                    </a>
-                    ) pour la remettre à l’inspecteur avant le début de l’examen.
-                  </li>
-                </ul>
+              Votre accompagnateur sera :
+              <ul>
+                <li>
+                  soit un enseignant de la conduite en possession de son
+                  autorisation d'enseigner pour la présenter à l'inspecteur,
+                </li>
+                <li>
+                  soit une personne dont le permis B est en cours de validité.
+                  Cette dernière devra remplir et signer la « charte de
+                  l’accompagnateur » (
+                  <a href="https://www.legifrance.gouv.fr/jo_pdf.do?id=JORFTEXT000036251681">
+                    https://www.legifrance.gouv.fr/jo_pdf.do?id=JORFTEXT000036251681
+                  </a>
+                  ) pour la remettre à l’inspecteur avant le début de l’examen.
+                </li>
+              </ul>
             </li>
             <li>
               Vous présenterez un titre d’identité en cours de validité : carte
-              nationale d’identité, passeport ou titre de séjour (liste complète :
-              {' '}
+              nationale d’identité, passeport ou titre de séjour (liste complète
+              :{' '}
               <a href="https://legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000033736411&categorieLien=id">
                 https://legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000033736411&categorieLien=id
               </a>
@@ -133,12 +148,11 @@ const Informations = () => (
               L'attestation d'assurance du véhicule, en cours de validité, à
               votre nom.
             </li>
-            <li> Une enveloppe affranchie à 20g.</li>
+            <li>Une enveloppe affranchie à 20 g.</li>
             <li>
               Lorsque vous avez fait l'objet d'une annulation du permis, le
-              récépissé de la « fiche retour au permis de conduire » que vous
-              aurez imprimé sur le site de l’ANTS :
-              {' '}
+              récépissé de la « fiche retour au permis de conduire » que vous
+              aurez imprimé sur le site de l’ ANTS :{' '}
               <a href="https://permisdeconduire.ants.gouv.fr/">
                 https://permisdeconduire.ants.gouv.fr/
               </a>
@@ -146,34 +160,20 @@ const Informations = () => (
             </li>
           </ul>
           <p>
-            Attention : le mauvais état du véhicule (pneus lisses, rétros cassés
-            ou absents, non fonctionnement de tous les feux, etc.), ou l'absence
-            ou la non-validité d'un des documents exigés ci-dessus, pour le
-            candidat ou pour l'accompagnateur, entraîne le report de l'examen à
-            une date ultérieure.
+            <strong>Attention</strong> : le mauvais état du véhicule (pneus
+            lisses, rétros cassés ou absents, non fonctionnement de tous les
+            feux, etc.), ou l'absence ou la non-validité d'un des documents
+            exigés ci-dessus, pour le candidat ou pour l'accompagnateur,
+            entraîne le report de l'examen à une date ultérieure.
           </p>
           <h2>Je ne trouve pas de place disponible</h2>
           <p>De nouveaux créneaux sont ajoutés chaque mois.</p>
           <h2>Je n'habite pas dans le 93</h2>
           <p>
             Ce service est restreint aux candidats domiciliés en
-            Seine-Saint-Denis.
-          </p>
-          <p>
-            Si vous n'habitez dans le 93, rapprochez-vous du Bureau de
-            l'éducation routière du département de votre domicile.
-          </p>
-          <h2>Qui sommes-nous ?</h2>
-          <p>
-            Nous sommes le Service des places d'examen de l'Unité Territoriale
-            de Seine-Saint-Denis.
-          </p>
-          <p>
-            Vous pouvez nous contacter par mail :
-            {' '}
-            <a href="mailto:candilib@interieur.gouv.fr">
-              candilib@interieur.gouv.fr
-            </a>
+            Seine-Saint-Denis qui ont reçu un mail d'invitation à participer à
+            cette expérimentation. Si vous n'habitez dans le 93, rapprochez-vous
+            du Bureau de l'éducation routière du département de votre domicile.
           </p>
         </div>
       </CardContent>
