e542a4b29de88fa2f6feac5ac7787fee0e5b1e19
Stanislas Ormière <stan@stormier.ninja>
2018-12-06 17:14:22 +0100

    Even more clean-up and npm dependencies upgrades

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -1,65 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
 
 // Import Style
 import styles from './App.css';
 
 // Import Components
-import Helmet from 'react-helmet';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
-// Import Actions
-import { toggleAddPost } from './AppActions';
-
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isMounted: false };
-  }
-
-  componentDidMount() {
-    this.setState({isMounted: true}); // eslint-disable-line
-  }
-
-  toggleAddPostSection = () => {
-    this.props.dispatch(toggleAddPost());
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <Helmet
-            title="Candlib Dev en cours"
-            titleTemplate="%s -candilib"
-            meta={[
-              { charset: 'utf-8' },
-              {
-                'http-equiv': 'X-UA-Compatible',
-                content: 'IE=edge',
-              },
-              {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-              },
-            ]}
-          />
-          <Header />
-          <div className={styles.container}>
-            {this.props.children}
-          </div>
-          <Footer />
-        </div>
+export const App = (props) => (
+  <div>
+    <div>
+      <Helmet
+        title="Candlib Dev en cours"
+        titleTemplate="%s -candilib"
+        meta={[
+          { charset: 'utf-8' },
+          {
+            'http-equiv': 'X-UA-Compatible',
+            content: 'IE=edge',
+          },
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1',
+          },
+        ]}
+      />
+      <Header />
+      <div className={styles.container}>
+        {props.children}
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  </div>
+);
 
 App.propTypes = {
   children: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired,
 };
 
 export default App;
