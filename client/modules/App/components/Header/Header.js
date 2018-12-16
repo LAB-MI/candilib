2d016789c8982b86e43827d87c3109f479777b69
Philippe Bron <philippe@bron.ovh>
2018-12-16 12:24:25 +0100

    ajout mention beta
    ajout bandeau version beta dans AppBar

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Toolbar, AppBar, Button } from '@material-ui/core';
+import { Toolbar, AppBar, Button, Grid } from '@material-ui/core';
 
 // Import Style
 import styles from './Header.css';
 
-import logo from '../../candilib_logo.png'
+import logo from '../../candilib_logo.png';
+import beta from '../../beta.png';
 
 function Header() {
   return (
     <div className={styles.header}>
       <AppBar color="default" position="static">
-        <Toolbar>
-          <img src={logo} width="200px" alt="logo" />
-          <Button component={Link} to="/">
-            Accueil
-          </Button>
-          <Button component={Link} to="/informations">
-            Informations
-          </Button>
-          <Button component={Link} to="/auth?redirect=calendar">
-            Ma Réservation
-          </Button>
-        </Toolbar>
+        <Grid container spacing={24}>
+          <Grid item xs={9}>
+              <Toolbar>
+                <img src={logo} style={{width: 200}} alt="logo" />
+                <Button component={Link} to="/">
+                  Accueil
+                </Button>
+                <Button component={Link} to="/informations">
+                  Informations
+                </Button>
+                <Button component={Link} to="/auth?redirect=calendar">
+                  Ma Réservation
+                </Button>
+              </Toolbar>
+          </Grid>
+          <Grid item xs={3}>
+              <img src={beta} style={{width: 60, height: 60,float: "right"}} alt="version beta" />
+          </Grid>
+        </Grid>
       </AppBar>
     </div>
   );
