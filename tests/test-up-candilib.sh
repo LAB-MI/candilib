4f22a7c3c382126426c81f9d8df35a38559ff1d3
pli01 <pli01@users.noreply.github.com>
2018-12-19 17:39:28 +0100

    Fix travis with new target

--- tests/test-up-candilib.sh
+++ tests/test-up-candilib.sh
@@ -15,7 +15,7 @@ timeout=120;
 test_result=1
 dirname=$(dirname $0)
 until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-        curl --fail --retry-max-time 120 --retry-delay 1  --retry 1  http://localhost:80
+        curl -L --fail --retry-max-time 120 --retry-delay 1  --retry 1  http://localhost:80
 	test_result=$?
 	echo "Wait $timeout seconds: ${APP} up $test_result";
 	(( timeout-- ))
--- tests/test-up-web.sh
+++ tests/test-up-web.sh
@@ -15,8 +15,9 @@ set +e
 timeout=120;
 test_result=1
 dirname=$(dirname $0)
+docker cp $dirname/fake-curl.sh ${APP}_$container_name:/tmp/
 until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-	${DC} -f ${DC_APP_RUN_PROD}  exec ${USE_TTY} $container_name curl --retry-max-time 120 --retry-delay 1  --retry 1 --fail http://localhost:8000 | grep 'Candilib'
+	${DC} -f ${DC_APP_RUN_PROD}  exec ${USE_TTY} $container_name /bin/bash /tmp/fake-curl.sh /candilib/ | grep 'Candilib'
 	test_result=$?
 	echo "Wait $timeout seconds: ${APP}-$container_name up $test_result";
 	(( timeout-- ))
